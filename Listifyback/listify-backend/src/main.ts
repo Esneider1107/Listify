efa1c46945c7007c930752ff53f7d7ec7b915462
Esneider1107 <esneiderjosue1107@gmail.com>
2025-11-24 17:36:40 -0500

    Update main.ts

--- Listifyback/listify-backend/src/main.ts
+++ Listifyback/listify-backend/src/main.ts
@@ -5,12 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   
-  app.enableCors({
-    origin: 'http://localhost:3001',
-    credentials: true,
-  });
-  
-  await app.listen(3000); // o tu puerto
+    app.enableCors({
+      origin: '*',
+    });
+  await app.listen(3000, '0.0.0.0');
+
 }
 
   bootstrap();
